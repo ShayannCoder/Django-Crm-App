1c76bb5a915234223219c7db5bc59e0f6920d202
Shayan Ebrahimi <shayan.ebrahimi@zcoderz.com>
2025-08-27 22:46:30 +0300

    removed the username field from the Record model.

--- website/models.py
+++ website/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class Record(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    username = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
