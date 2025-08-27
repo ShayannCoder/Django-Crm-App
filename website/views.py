9567ef2ab7be032ff6ced8321ad8f26fac0ec283
Shayan Ebrahimi <shayan.ebrahimi@zcoderz.com>
2025-08-28 01:38:37 +0300

    created the view function for adding a new record of customer

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, AddRecord
 from .models import Record
 
 # Create your views here.
@@ -23,7 +23,7 @@ def register_user(request):
             password = form.cleaned_data["password1"]
             user = authenticate(request, username=username, password=password)
             login(request, user)
-            messages.success(request, "You Have Successfully Registered!")
+            messages.success(request, "You Have Registered Successfully!")
             return redirect("home")
     else:
         form = UserRegistrationForm()
@@ -68,8 +68,26 @@ def delete_record(request, pk):
     if request.user.is_authenticated:
         delete_record = Record.objects.get(id=pk)
         delete_record.delete()
-        messages.success(request, "Record Successfully Deleted.")
+        messages.success(request, "Record Deleted Successfully.")
         return redirect("home")
     else:
         messages.success(request, "You Must Be Logged In To Delete a Record.")
         return redirect("login")
+    
+
+def add_record(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            new_record = AddRecord(request.POST)
+            if new_record.is_valid():
+                new_record.save()
+                messages.success(request, "New Record Added Successfully.")
+                return redirect("home")
+        else:
+            new_record = AddRecord()
+        context = {"new_record":new_record}
+        return render(request, "add_record.html", context)
+    else:
+        messages.success(request, "You Must Be Logged In To Add a Record.")
+        return redirect("login")
+    
