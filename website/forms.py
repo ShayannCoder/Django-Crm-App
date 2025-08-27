ddbfea9800cee271b42c8b8cfd058002f2e2cf9c
Shayan Ebrahimi <shayan.ebrahimi@zcoderz.com>
2025-08-28 01:32:03 +0300

    created the form for adding a new record

--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(max_length=100)
@@ -10,3 +11,18 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
+
+
+class AddRecord(forms.Form):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    phone_number = forms.CharField(max_length=15)
+    address = forms.CharField(max_length=100)
+    city = forms.CharField(max_length=100)
+    state = forms.CharField(max_length=100)
+    zipcode =forms.CharField(max_length=100)
+
+    class Meta:
+        model = Record
+        fields = ["first_name", "last_name", "email", "phone_number", "address", "city", "state", "zipcode"]
