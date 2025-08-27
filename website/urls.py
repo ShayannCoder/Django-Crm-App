3fadef41d5918f686ccb32db209cd2e9939f6332
Shayan Ebrahimi <shayan.ebrahimi@zcoderz.com>
2025-08-28 01:03:39 +0300

    created a new path for deleting a record

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("logout/", views.logout_user, name="logout"),
     path("register/", views.register_user, name="register"),
     path("records/<int:pk>", views.customer_record, name="customer_record"),
+    path("records/delete/<int:pk>", views.delete_record, name="delete_record"),
 ]
