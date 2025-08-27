6df082832e30ab22278db78a59e5f205c924ded0
Shayan Ebrahimi <shayan.ebrahimi@zcoderz.com>
2025-08-27 22:29:12 +0300

    created a user registration form and page by using crispy forms, users can successfully register and login, with correct popups of error messages if it occurs.

--- website/views.py
+++ website/views.py
@@ -2,12 +2,30 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm
 
 # Create your views here.
 
 @login_required
 def home(request):
     return render(request, "home.html")
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered!")
+            return redirect("home")
+    else:
+        form = UserRegistrationForm()
+    context = {"form":form}
+    return render(request, "register.html", context)
 
 
 def login_user(request):
